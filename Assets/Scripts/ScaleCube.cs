ceca9075d3174e3e30390c7481084958a105d647
Dominic Tsoi <dtsoi@calpoly.edu>
2020-02-22 14:56:21 -0800

    integrating code

--- Assets/Scripts/ScaleCube.cs
+++ Assets/Scripts/ScaleCube.cs
@@ -16,16 +16,16 @@ public class ScaleCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*audioInfluence = Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.LowPass);
+        audioInfluence = Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.LowPass);
         audioInfluenceVec = new Vector3(1 + audioInfluence, 1 + audioInfluence, 1 + audioInfluence);
         transform.localScale = audioInfluenceVec;
-        print(audioInfluence);*/
+        print(audioInfluence);
     }
 
     public void changeScale(float influence)
     {
-        audioInfluenceVec = new Vector3(1 + influence, 1 + influence, 1 + influence);
+        /*audioInfluenceVec = new Vector3(1 + influence, 1 + influence, 1 + influence);
         transform.localScale = audioInfluenceVec;
-        print(audioInfluence);
+        print(audioInfluence);*/
     }
 }
