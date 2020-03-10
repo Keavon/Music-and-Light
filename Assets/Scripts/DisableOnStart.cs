6f1782260c61615b3e3a2acc8a7a3ddb8cc6ab82
Keavon Chambers <keavon@keavon.com>
2020-03-10 14:54:02 -0700

    Fixed issues relating to library video screen rendering scene

---
+++ Assets/Scripts/DisableOnStart.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnStart : MonoBehaviour {
+	void Start() {
+		gameObject.SetActive(false);
+	}
+}
