2df228a13f41f04e9ec09c8f0aeba87990eb211d
Ellen Liu <allenliu789@gmail.com>
2020-05-11 14:18:47 -0700

    Changed audio reactive component of particle systems to be in velocity

--- Assets/Scripts/ParticleAudioReact.cs
+++ Assets/Scripts/ParticleAudioReact.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ParticleAudioReact : MonoBehaviour
 {
     public float audioInfluence = 0.0f;
+    float minSpeed = 0.1f;
+    float maxSpeed = 5.0f;
     private ParticleSystem pSys;
     ParticleSystem.Particle[] mParticles;
     // Start is called before the first frame update
@@ -18,19 +20,31 @@ public class ParticleAudioReact : MonoBehaviour
     void Update()
     {
         var main = pSys.main;
-        audioInfluence = Mathf.Clamp(Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.LowPass) * 3,0,1);
+        audioInfluence = Mathf.Clamp(Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.LowPass) * 5,0,1);
         if (mParticles == null)
         {
             mParticles = new ParticleSystem.Particle[pSys.main.maxParticles];
         }
         int nump = pSys.GetParticles(mParticles);
         ParticleSystem.Particle newPart;
-        Color newColor = new Color(Mathf.Clamp(audioInfluence - Random.Range(0.2f,0.5f),0.0f,1.0f), Mathf.Clamp(audioInfluence - Random.Range(0.2f, 0.5f), 0.0f, 1.0f), audioInfluence);
+        //Color newColor = new Color(Mathf.Clamp(audioInfluence - Random.Range(0.2f,0.5f),0.0f,1.0f), Mathf.Clamp(audioInfluence - Random.Range(0.2f, 0.5f), 0.0f, 1.0f), audioInfluence);
         for (int i = 0; i < nump; i++)
         {
             newPart = mParticles[i];
-            newPart.startColor = newColor;
-            newPart.startSize = Random.Range(0.05f, 0.15f);
+            //newPart.startColor = newColor;
+            //newPart.startSize = Random.Range(0.05f, 0.15f);
+            Vector3 addVelDir = mParticles[i].velocity.normalized;
+            newPart.velocity -= 0.5f * addVelDir * Time.deltaTime;
+            if (audioInfluence > 0.8)
+            {
+                newPart.velocity += 0.8f *addVelDir * Time.deltaTime;
+                //Debug.Log("Adding Velocity");
+            }
+            if (newPart.velocity.magnitude < minSpeed)
+                newPart.velocity = minSpeed * addVelDir;
+            if (newPart.velocity.magnitude > maxSpeed)
+                newPart.velocity = maxSpeed * addVelDir;
+            Debug.Log("Current speed: " + newPart.velocity.magnitude);
             mParticles[i] = newPart;
         }
         pSys.SetParticles(mParticles, nump);
