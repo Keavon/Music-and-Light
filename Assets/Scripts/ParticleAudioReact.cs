9b319ec701bd6232b60dd7231acb93bebb2d4697
Ellen Liu <allenliu789@gmail.com>
2020-05-13 15:20:27 -0700

    Changed Audio filter to Bypass

--- Assets/Scripts/ParticleAudioReact.cs
+++ Assets/Scripts/ParticleAudioReact.cs
@@ -7,6 +7,7 @@ public class ParticleAudioReact : MonoBehaviour
     public float audioInfluence = 0.0f;
     float minSpeed = 0.1f;
     float maxSpeed = 5.0f;
+    float baseVelocity = 5.0f;
     private ParticleSystem pSys;
     ParticleSystem.Particle[] mParticles;
     // Start is called before the first frame update
@@ -20,7 +21,7 @@ public class ParticleAudioReact : MonoBehaviour
     void Update()
     {
         var main = pSys.main;
-        audioInfluence = Mathf.Clamp(Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.LowPass) * 5,0,1);
+        audioInfluence = Mathf.Clamp(Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.Bypass) * 2,0,1);
         if (mParticles == null)
         {
             mParticles = new ParticleSystem.Particle[pSys.main.maxParticles];
@@ -34,16 +35,17 @@ public class ParticleAudioReact : MonoBehaviour
             //newPart.startColor = newColor;
             //newPart.startSize = Random.Range(0.05f, 0.15f);
             Vector3 addVelDir = mParticles[i].velocity.normalized;
-            newPart.velocity -= 0.5f * addVelDir * Time.deltaTime;
-            if (audioInfluence > 0.8)
-            {
-                newPart.velocity += 0.8f *addVelDir * Time.deltaTime;
-                //Debug.Log("Adding Velocity");
-            }
+            //newPart.velocity -= 0.5f * addVelDir * Time.deltaTime;
+            //if (audioInfluence > 0.8)
+            //{
+            //    newPart.velocity += 0.8f *addVelDir * Time.deltaTime;
+            //    //Debug.Log("Adding Velocity");
+            //}
+            newPart.velocity = addVelDir * baseVelocity * audioInfluence;
             if (newPart.velocity.magnitude < minSpeed)
                 newPart.velocity = minSpeed * addVelDir;
-            if (newPart.velocity.magnitude > maxSpeed)
-                newPart.velocity = maxSpeed * addVelDir;
+            //if (newPart.velocity.magnitude > maxSpeed)
+            //    newPart.velocity = maxSpeed * addVelDir;
             Debug.Log("Current speed: " + newPart.velocity.magnitude);
             mParticles[i] = newPart;
         }
