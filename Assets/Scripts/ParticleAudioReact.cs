08894fb0bb3df57044ba0a69823a2a189299c89d
Dominic Tsoi - ADMIN <dtsoi-admin@calpoly.edu>
2020-05-13 18:00:54 -0700

    tweaked particle system for audio reactivity and added and tweaked a sub emitter any time the particle hits a collider.

--- Assets/Scripts/ParticleAudioReact.cs
+++ Assets/Scripts/ParticleAudioReact.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ParticleAudioReact : MonoBehaviour
 {
     public float audioInfluence = 0.0f;
-    float minSpeed = 0.1f;
-    float maxSpeed = 5.0f;
-    float baseVelocity = 5.0f;
+    float minSpeed = 0.01f;
+    float maxSpeed = 4.0f;
+    float baseVelocity = 2.0f;
     private ParticleSystem pSys;
     ParticleSystem.Particle[] mParticles;
+    public GameObject particleSubspawn;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public float velocityFactor = 1.0f;
+    public float minStartSize = 0.5f;
+    public float maxStartSize = 7.0f;
     // Start is called before the first frame update
     void Start()
     {
         pSys = GetComponent<ParticleSystem>();
-
+        collisionEvents = new List<ParticleCollisionEvent>();
     }
 
     // Update is called once per frame
@@ -28,12 +33,20 @@ public class ParticleAudioReact : MonoBehaviour
         }
         int nump = pSys.GetParticles(mParticles);
         ParticleSystem.Particle newPart;
-        //Color newColor = new Color(Mathf.Clamp(audioInfluence - Random.Range(0.2f,0.5f),0.0f,1.0f), Mathf.Clamp(audioInfluence - Random.Range(0.2f, 0.5f), 0.0f, 1.0f), audioInfluence);
+        Color newColor = new Color(Mathf.Clamp(audioInfluence - Random.Range(0.2f,0.5f),0.0f,1.0f), Mathf.Clamp(audioInfluence - Random.Range(0.2f, 0.5f), 0.0f, 1.0f), audioInfluence);
         for (int i = 0; i < nump; i++)
         {
             newPart = mParticles[i];
-            //newPart.startColor = newColor;
-            //newPart.startSize = Random.Range(0.05f, 0.15f);
+            newPart.startColor = newColor;
+            newPart.startSize = Random.Range(0.05f * (0.5f + audioInfluence * audioInfluence) , 0.15f * (0.5f + audioInfluence));
+            if (newPart.startSize < minStartSize)
+            {
+                newPart.startSize = minStartSize;
+            }
+            if (newPart.startSize > maxStartSize)
+            {
+                newPart.startSize = maxStartSize;
+            }
             Vector3 addVelDir = mParticles[i].velocity.normalized;
             //newPart.velocity -= 0.5f * addVelDir * Time.deltaTime;
             //if (audioInfluence > 0.8)
@@ -41,7 +54,7 @@ public class ParticleAudioReact : MonoBehaviour
             //    newPart.velocity += 0.8f *addVelDir * Time.deltaTime;
             //    //Debug.Log("Adding Velocity");
             //}
-            newPart.velocity = addVelDir * baseVelocity * audioInfluence;
+            newPart.velocity = addVelDir * baseVelocity * (audioInfluence * velocityFactor);
             if (newPart.velocity.magnitude < minSpeed)
                 newPart.velocity = minSpeed * addVelDir;
             //if (newPart.velocity.magnitude > maxSpeed)
@@ -51,5 +64,20 @@ public class ParticleAudioReact : MonoBehaviour
         }
         pSys.SetParticles(mParticles, nump);
         //Debug.Log("NewColor: (" + newColor.r + ", " + newColor.g + ", " + newColor.b + ")");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = pSys.GetCollisionEvents(other, collisionEvents);
+        int i = 0;
+        while (i < numCollisionEvents)
+        {
+            Vector3 collisionHitLoc = collisionEvents[i].intersection;
+            if (audioInfluence > 0.001)
+            {
+                Instantiate(particleSubspawn, collisionHitLoc, Quaternion.identity);
+            }
+            i++;
+        }
     }
 }
