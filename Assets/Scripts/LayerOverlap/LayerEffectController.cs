2b212a4bf6602c46ccb5aa3e983a56d4eee1bfa8
Dominic Tsoi - ADMIN <dtsoi-admin@calpoly.edu>
2020-05-27 20:22:13 -0700

    added audio reactivity to the box spawner

--- Assets/Scripts/LayerOverlap/LayerEffectController.cs
+++ Assets/Scripts/LayerOverlap/LayerEffectController.cs
@@ -14,12 +14,13 @@ public class LayerEffectController : MonoBehaviour
 
     public GameObject layerPrefab;
 
+    public float audioInfluence = 0.0f;
     // Start is called before the first frame update
     void Awake()
     {
 
         if (layerPrefab == null) {
-            //Debug.LogError("Forgot to set layer prefab");
+            //  Debug.LogError("Forgot to set layer prefab");
         }
 
         if (autoFind) {
@@ -37,6 +38,47 @@ public class LayerEffectController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        audioInfluence = Mathf.Clamp(Lasp.MasterInput.GetPeakLevel(Lasp.FilterType.LowPass) * 8, 0, 3);
+        if (audioInfluence > 0.35)
+        {
+            Debug.Log("trigger");
+            int roundedRandom = (int)Mathf.Round(audioInfluence);
+            foreach (LayerSpawnerZones spawner in spawners)
+            {
+                // float audioRandom = Random.Range(0.0f, audioInfluence);
+                // int roundedRandom = (int)Mathf.Round(audioRandom);
+                if (roundedRandom == 0)
+                {
+                    spawner.StartLayer(LayerSpawnerZones.Directions.North, GenerateColor(audioInfluence / 3), roundedRandom / 4);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.South, GenerateColor(audioInfluence / 3), roundedRandom / 4);
+                }
+                else if (roundedRandom == 1)
+                {
+                    spawner.StartLayer(LayerSpawnerZones.Directions.South, GenerateColor(audioInfluence / 3), roundedRandom * roundedRandom / 2);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.North, GenerateColor(audioInfluence / 3), roundedRandom / 2);
+                }
+                else if (roundedRandom == 2)
+                {
+                    spawner.StartLayer(LayerSpawnerZones.Directions.North, GenerateColor(audioInfluence / 3.5f), roundedRandom);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.West, GenerateColor(audioInfluence / 3.2f), roundedRandom);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.East, GenerateColor(audioInfluence / 3.1f), roundedRandom * roundedRandom);
+                }
+                else if (roundedRandom == 3)
+                {
+                    spawner.StartLayer(LayerSpawnerZones.Directions.North, GenerateColor(audioInfluence / 3.05f), roundedRandom * roundedRandom / 2);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.East, GenerateColor(audioInfluence / 3.5f), roundedRandom);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.West, GenerateColor(audioInfluence / 3.9f), roundedRandom * roundedRandom);
+                    spawner.StartLayer(LayerSpawnerZones.Directions.South, GenerateColor(audioInfluence / 3.4f), roundedRandom * roundedRandom / 4);
+                }
+            }
+        }
     }
+
+
+    private Color GenerateColor(float val) { 
+        return new Color(Mathf.Clamp((val / 2) + Randomization.RandomFloat(0, 1), 0, 1),
+                         Mathf.Clamp((val / 2) + Randomization.RandomFloat(0, 1), 0 , 1),
+                         Mathf.Clamp((val / 2) + Randomization.RandomFloat(0, 1), 0 , 1), 1);
+    }
+
 }
--- Assets/Scripts/LayerOverlap/MoveBack.cs
+++ Assets/Scripts/LayerOverlap/MoveBack.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class MoveBack : MonoBehaviour
 {
 
-    public float speed = 0.02f;
+    public float speed = 0.12f;
+    public float speedMin = 0.08f;
+    public float speedMax = 0.18f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = UnityEngine.Random.Range(speedMin, speedMax);
     }
 
     // Update is called once per frame
